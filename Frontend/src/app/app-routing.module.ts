ef586a8a4133de996ea7f5903f6fda47e947c486
julianomartinsluz <julianomartins769@gmail.com>
2022-10-18 21:02:41 -0300

    fazendo forms, css nos components, criando services

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent},
 ];
 
 @NgModule({
